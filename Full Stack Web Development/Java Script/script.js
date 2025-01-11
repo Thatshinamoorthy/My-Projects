440a76a07ddee36d0b46e677d81427bb90fdfe09
Thatshinamoorthy <rthatshinamoorthy@gmail.com>
2025-01-11 13:40:18 +0530

    Learn : java Script

---
+++ Full Stack Web Development/Java Script/script.js
@@ -0,0 +1,42 @@
+//This is external java script code that linked in index.js
+
+//alert element
+alert("this is alert message");
+
+//Data Types
+//1. Number
+//2.String
+//3.Array
+//4. Object
+//These are mostly used data types in java script
+
+//Variable Declaration
+var Name  = "Thatshinamoorthy";
+var age = 19;
+
+//print statement in js one is alert and another is console.log()
+console.log(Name);
+console.log(age);
+
+//Company Standard variable declaration
+
+var MyName = "Thatshinamoorthy";//partial case
+
+var myName = "Thatshinamoorthy";//camel case
+
+var my_name = "Thatshinamoorthy";//under score
+
+// Array Declaration
+
+var fruits = ["Apple","Orange","Mango","Banana"]
+
+console.log(fruits[1]);
+
+var num = [2,5,9,3,7,4]
+console.log(num);
+console.log(num[2]);
+console.log(num.sort());
+console.log(num.reverse());
+console.log(num.length);
+num[1] = 1;//Alter the value of index 1
+num.push(6);//push value to the num array at the end of array
